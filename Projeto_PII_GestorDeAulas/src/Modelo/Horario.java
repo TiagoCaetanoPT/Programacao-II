ccccf811c29cfa9288299ccf6dccb32f7ad1c48c
TiagoCaetanoPT <tiago.o.caetano@gmail.com>
2019-03-13 18:12:06 +0000

    Fim da aula 4

---
+++ Projeto_PII_GestorDeAulas/src/Modelo/Horario.java
@@ -0,0 +1,20 @@
+package Modelo;
+
+public class Horario {
+
+    private DiaSemana diaSemana;
+    private  Integer horaInicio;
+    private  Integer duracao;
+
+    public DiaSemana getDiaSemana() {
+        return diaSemana;
+    }
+
+    public Integer getHoraInicio() {
+        return horaInicio;
+    }
+
+    public Integer getDuracao() {
+        return duracao;
+    }
+}
